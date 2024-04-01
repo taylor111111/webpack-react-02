d91abb9a43be75b81fd78e5c4cb9e55113c6cf25
xiaoyue hu <xiaoyuehu@xiaoyues-Mac-Studio.local>
2024-04-01 11:56:10 +0800

    feat: use webpack serve

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.config.js');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: 'development',
+    output: {
+        filename: 'js/[name].[hash:8].bundle.js',
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            inject: 'body',
+            hash: false
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+});
