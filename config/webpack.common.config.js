e6e238878a6518c39f6c7ffb5c94cc5b7ecba562
xiaoyue hu <xiaoyuehu@xiaoyues-Mac-Studio.local>
2024-04-01 09:49:54 +0800

    add css loader

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -14,6 +14,14 @@ module.exports = {
         test: /\.(js|jsx)$/,
         use: 'babel-loader',
         exclude: /node_modules/,//不需要去转译"node\_modules"这里面的文件。模块发布到npm时都要提供源码以及编译后的commonjs规范的代码
+      },
+      {
+        test: /\.css$/,
+        use: [
+
+          'style-loader',// 最后计算完的css，将会使用style-loader生成一个内容为最终解析完的css代码的style标签，放到head标签里
+          'css-loader' // css-loader加载器去解析这个文件，遇到“@import”等语句就将相应样式文件引入
+        ]
       }
     ]
   }
